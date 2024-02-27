c3e9964d3245356881672e4959d58500ecff3f16
lamia1998 <lamiaachrih1998@gmail.com>
2024-02-27 01:38:21 -0800

    team project

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,26 +15,26 @@ cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(self):
-    """ handles teardown """
-    storage.close()
+        """ handles teardown """
+            storage.close()
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """ handles 404 errors """
-    status = {"error": "Not found"}
-    return jsonify(status), 404
+            @app.errorhandler(404)
+            def page_not_found(e):
+                    """ handles 404 errors """
+                        status = {"error": "Not found"}
+                            return jsonify(status), 404
 
 
-if __name__ == '__main__':
-    try:
-        host = os.environ.get('HBNB_API_HOST')
-    except:
-        host = '0.0.0.0'
+                        if __name__ == '__main__':
+                                try:
+                                            host = os.environ.get('HBNB_API_HOST')
+                                                except:
+                                                            host = '0.0.0.0'
 
-    try:
-        port = os.environ.get('HBNB_API_PORT')
-    except:
-        port = '5000'
+                                                                try:
+                                                                            port = os.environ.get('HBNB_API_PORT')
+                                                                                except:
+                                                                                            port = '5000'
 
-    app.run(host=host, port=port)
+                                                                                                app.run(host=host, port=port)
