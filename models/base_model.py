f67ff3713577233cd5d3db4b5d41cb030a209bc6
TheYaksha00 <aymanbahla69@gmail.com>
2024-02-27 12:36:47 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,6 @@
-ontains class BaseModel
+#!/usr/bin/python3
+"""
+Coontains class BaseModel
 """
 
 from datetime import datetime
@@ -56,7 +58,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_fs=None):
+    def to_dict(self, save_check=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -66,9 +68,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if save_fs is None:
-            if "password" in new_dict:
-                del new_dict["password"]
+        if "password" in new_dict and save_check is False:
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
--- models/user.py
+++ models/user.py
@@ -1,13 +1,12 @@
-
-lds class User"""
-
+#!/usr/bin/python3
+""" holds class User"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from hashlib import md5
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -29,9 +28,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-
-    def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
+        if self.password:
+            hashed_password = hashlib.md5(self.password.encode("utf-8"))
+            self.password = hashed_password.hexdigest()
